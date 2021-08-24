4742a866f004b18fb2f681dfafb375b94aba075c
Peter-Xidian <mayowaodebode@gmail.com>
2021-08-24 14:58:55 +0200

    JS Commit 2nd

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -7,15 +7,6 @@ function scrollHeader() {
 }
 window.addEventListener("scroll", scrollHeader);
 
-/*==================== SHOW SCROLL TOP ====================*/
-function scrollTop() {
-  const scrollTop = document.getElementById("scroll-top");
-  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-  if (this.scrollY >= 560) scrollTop.classList.add("show-scroll");
-  else scrollTop.classList.remove("show-scroll");
-}
-window.addEventListener("scroll", scrollTop);
-
 // /*==================== MENU SHOW Y HIDDEN ====================*/
 // const navMenu = document.getElementById("rnav-menu"),
 //   toggleMenu = document.getElementById("nav-toggle"),
@@ -103,3 +94,12 @@ sr.reveal(
     interval: 200,
   }
 );
+
+/*==================== SHOW SCROLL TOP ====================*/
+function scrollTop() {
+  const scrollTop = document.getElementById("scroll-top");
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 560) scrollTop.classList.add("show-scroll");
+  else scrollTop.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollTop);
